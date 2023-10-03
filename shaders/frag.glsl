a74d4c8e5e96c9cad2eaafc45d6a7baa21a377bc
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-10-03 16:51:55 -0400

    Adds the fragment shader

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,19 +1,16 @@
 #version 410 core
-in vec3 fragNormal;
+in vec2 fragTexCoord;
+uniform float time; // Updated per frame in your C++ code
+uniform vec2 interactionPoint; // Updated based on user interaction in your C++ code
 
-out vec4 color;
-
-uniform bool isWireframeMode;
+out vec4 fragColor;
 
 void main()
 {
+    float distance = length(fragTexCoord - interactionPoint);
+    float ripple = sin(20.0 * (distance - time));
+    float rippleIntensity = 0.1 * ripple / distance;
     
-    if(isWireframeMode) {
-        color = vec4(1.0, 1.0, 1.0, 1.0);
-    } else {
-        vec3 normalizedNormal = normalize(fragNormal);
-        color = vec4(normalizedNormal * 0.5 + 0.5, 1.0);
-    }
-    
-    
+    vec3 color = vec3(0.0, 0.5 + rippleIntensity, 1.0); // Simple blue color with ripple intensity
+    fragColor = vec4(color, 1.0);
 }
