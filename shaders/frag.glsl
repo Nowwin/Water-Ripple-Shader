ba1f6109a6bdd2a318d96fa790f0899482c57ae5
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-10-03 20:11:03 -0400

    Adds smooth effects and decay functions

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,7 +1,9 @@
 #version 410 core
 in vec2 fragTexCoord;
-uniform float time; // Updated per frame in your C++ code
-uniform vec2 interactionPoint; // Updated based on user interaction in your C++ code
+uniform float time; // Updated per frame
+uniform vec2 interactionPoint; // Updated based on user interaction
+uniform float rippleStartTime;  // Time the ripple was created
+uniform float decayRate;  // Decay rate
 
 out vec4 fragColor;
 
@@ -9,8 +11,11 @@ void main()
 {
     float distance = length(fragTexCoord - interactionPoint);
     float ripple = sin(20.0 * (distance - time));
-    float rippleIntensity = 0.1 * ripple / distance;
-    
-    vec3 color = vec3(0.0, 0.5 + rippleIntensity, 1.0); // Simple blue color with ripple intensity
+    float decayFactor = exp(-decayRate * (time - rippleStartTime));
+    float rippleIntensity = 0.1 * ripple / distance * decayFactor;
+    //float rippleIntensity = 0.1 * ripple / distance;
+    float softRipple = smoothstep(0.0, 1.0, rippleIntensity);
+    vec3 color = vec3(0.0, 0.5 + softRipple, 1.0);
+
     fragColor = vec4(color, 1.0);
 }
